e4c007c19b5495fa0459955e66e0f31c53d962b2
ZoeBijl <github@hello.moiety.me>
2022-02-14 22:52:57 +0100

    fix: add vertical bounding checks

--- tooltips.js
+++ tooltips.js
@@ -3,11 +3,25 @@ class Tooltip {
         this.container = element
         this.button = element.querySelector('button')
         this.tooltip = element.querySelector('[role=tooltip]')
+        this.tooltipPosition = this.getTooltipPosition()
         this.globalEscapeBound = this.globalEscape.bind(this)
         this.globalPointerDownBound = this.globalPointerDown.bind(this)
+        this.initialiseClassList()
         this.bindEvents()
     }
+
+    // Basic actions
+    open() {
+        this.showTooltip()
+        this.attachGlobalListener()
+    }
     
+    close() {
+        this.hideTooltip()
+        this.removeGlobalListener()
+    }
+    
+    // Binding event listteners
     bindEvents() {
         // Events that trigger open()
         // Open on mouse hover
@@ -22,16 +36,6 @@ class Tooltip {
         this.container.addEventListener('mouseleave', this.close.bind(this))
         // Close when the buttons loses focus
         this.button.addEventListener('blur', this.close.bind(this))
-    }
-    
-    open() {
-        this.showTooltip()
-        this.attachGlobalListener()
-    }
-    
-    close() {
-        this.hideTooltip()
-        this.removeGlobalListener()
     }
     
     attachGlobalListener() {
@@ -64,6 +68,7 @@ class Tooltip {
         }
     }
     
+    // Show or hide the tooltip
     showTooltip() {
         this.container.classList.add('tooltip-visible')
         this.tooltip.classList.remove('hidden')
@@ -76,32 +81,104 @@ class Tooltip {
         this.resetBoundingBox()
     }
 
+    // Get the desired default position for the tooltip (defaults to 'bottom')
+    getTooltipPosition() {
+        let attribute = this.container.getAttribute('data-tooltip-position')
+        let setting = 'bottom'
+
+        if (attribute === 'top') {
+            setting = attribute
+        }
+
+        return setting;
+    }
+
+    // Set the default classes for tooltips based on this.getTooltipPosition()
+    initialiseClassList() {
+        switch (this.tooltipPosition) {
+            case 'top':
+                this.container.classList.add('top')
+                break
+        
+            default:
+                this.container.classList.remove('top')
+                break
+        }
+    }
+
+    // Calculate if the tooltip is within the viewport
     checkBoundingBox() {
-        let windowWidth = window.innerWidth
         let bounds = this.tooltip.getBoundingClientRect()
 
+        this.checkHorizontalBounding(bounds)
+        this.checkVerticalBounding(bounds)
+    }
+
+    checkHorizontalBounding(bounds) {
+        let windowWidth = window.innerWidth
+    
+        // If the tooltip overlaps on both sides, throw an error
+        if (bounds.right > windowWidth && bounds.left < 0) {
+            throw new Error('Tooltip width too wide for the window')
+        }
+
+        // Check if the right side of the tooltip is beyond the right side of the viewport
         if (bounds.right > windowWidth) {
             this.moveTooltipLeft(bounds, windowWidth)
-        } else if (bounds.left < 0 ) {
+        }
+
+        // Check if the left side of the tooltip is beyond the left side of the viewport
+        if (bounds.left < 0 ) {
             this.moveTooltipRight(bounds)
         }
     }
+
+    checkVerticalBounding(bounds) {
+        let windowHeight = window.innerHeight
     
-    moveTooltipLeft(bounds, windowWidth) {
-        let translateAmount = (windowWidth - Math.round(bounds.right) - (Math.round(bounds.width) / 1.6))
-        this.tooltip.style.transform = `translateX(${translateAmount}px)`
+        // If the tooltip overlaps on both sides, throw an error
+        if (bounds.bottom > windowHeight && bounds.top < 0) {
+            throw new Error('Tooltip height too high for the window')
+        }
+
+        // Check if the bottom of the tooltip is below the bottom of the viewport
+        if (bounds.bottom > windowHeight) {
+            this.moveTooltipUp()
+        }
+
+        // Check if the top of the tooltip is above the top of the viewport
+        if (bounds.top < 0) {
+            this.moveTooltipDown()
+        }
     }
-    
+
+    // Move the tooltip so it fits within the viewport
+    moveTooltipUp() {
+        this.container.classList.add('top')
+    }
+
     moveTooltipRight(bounds) {
         let numToMove = Math.floor(bounds.width / 2)
         this.tooltip.style.left = `${numToMove}px`
     }
 
+    moveTooltipDown() {
+        this.container.classList.remove('top')
+    }
+
+    moveTooltipLeft(bounds, windowWidth) {
+        let translateAmount = (windowWidth - Math.round(bounds.right) - (Math.round(bounds.width) / 1.6))
+        this.tooltip.style.transform = `translateX(${translateAmount}px)`
+    }
+
+    // Reset the changes made by the bounding box functions
     resetBoundingBox() {
-        if (this.tooltip.style.left || this.tooltip.style.transform) {
-            this.tooltip.style.left = null
-            this.tooltip.style.transform = null
+        if (tooltip.style.left || tooltip.style.transform) {
+            tooltip.style.left = null
+            tooltip.style.transform = null
         }
+
+        this.initialiseClassList()
     }
 }
 
